112153427c08755eff16cc3ba8e54f8cf4dcd048
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-03-23 09:39:31 -0300

    corrigido Time Zone: America/Fortaleza.

--- deploy_qt/k8sRun.sh
+++ deploy_qt/k8sRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo unlink /etc/localtime;
-sudo ln -s /usr/share/zoneinfo/Etc/GMT+2 /etc/localtime;
+sudo ln -s /usr/share/zoneinfo/America/Fortaleza /etc/localtime
 export LD_LIBRARY_PATH=/home/user/qtlibs:/home/user/app;
 export QT_PLUGIN_PATH=/home/user/qtlibs/plugins;
 export QT_QPA_PLATFORM_PLUGIN_PATH=$QT_QPA_PLATFORM_PLUGIN_PATH:/home/user/qtlibs
