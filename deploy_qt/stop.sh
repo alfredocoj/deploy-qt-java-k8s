61e9bf0cc238d7fe31b7208f3ce6b49c1c38267e
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-07-09 17:23:33 -0300

    fixed stop.sh - delete deployment and delete file lastversion.txt and lastversiondocker.txt

--- deploy_qt/stop.sh
+++ deploy_qt/stop.sh
@@ -14,11 +14,19 @@ then
     K8sAppName=${mainName,,}
 fi
 
-#ssh k8s-admin@192.168.6.95 eval "kubectl delete service/$K8sAppName-service --namespace=qt-$env"
+ssh k8s-admin@192.168.6.95 eval "kubectl delete service/$K8sAppName-service --namespace=qt-$env"
 ssh k8s-admin@192.168.6.95 eval "kubectl delete deployment/$K8sAppName --namespace=qt-$env"
+
+DockerAppDir=/usr/local/appversion/release/$env/$mainName$lastName
+
+echo "$DockerAppDir"
+## Arquivo com a última versão da aplicação e release selecionadas
+ssh k8s-admin@192.168.6.95 eval "rm -rf $DockerAppDir/lastversion.txt"
+## Arquivo com a última versão da imagem docker para a aplicação e release selecionadas
+ssh k8s-admin@192.168.6.95 eval "rm -rf $DockerAppDir/lastversiondocker.txt"
 
 echo "Parada de aplicação realizado com sucesso!"
 
-echo "Para reinciar a aplicação, você deve chamar o script uploadqt.sh passando um último parametro chamado 'restart'";
+#echo "Para reinciar a aplicação, você deve chamar o script uploadqt.sh passando um último parametro chamado 'restart'";
 
-echo "Após o restart da aplicação, não se esqueça de retirar o parâmetro 'restart'";
+#echo "Após o restart da aplicação, não se esqueça de retirar o parâmetro 'restart'";
