d35151a8bbf435e7335a42d34d8e4f6319d60bc5
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-07-09 16:14:20 -0300

    export to environment LD_LIBRARY_PATH to directory of oracle driver.

--- deploy_qt/k8sRun.sh
+++ deploy_qt/k8sRun.sh
@@ -2,7 +2,7 @@
 
 sudo unlink /etc/localtime;
 sudo ln -s /usr/share/zoneinfo/America/Fortaleza /etc/localtime
-export LD_LIBRARY_PATH=/home/user/qtlibs:/home/user/app;
+export LD_LIBRARY_PATH=/home/user/qtlibs/oracle:/home/user/qtlibs:/home/user/app;
 export QT_PLUGIN_PATH=/home/user/qtlibs/plugins;
 export QT_QPA_PLATFORM_PLUGIN_PATH=$QT_QPA_PLATFORM_PLUGIN_PATH:/home/user/qtlibs
 #export XDG_RUNTIME_DIR=/tmp/runtime-developer
